31f3e9550eb0919d9857008e92920e8b360a43f6
Praneeth Bhatt <psbhatt@ananke.cesb.uky.edu>
2021-06-11 18:28:20 -0400

    made code more efficient

--- kendalltau.py
+++ kendalltau.py
@@ -3,30 +3,22 @@ import scipy.stats as sci
 
 
 def runNumpyKT(x, y, type='global'):
-    """Finds missing values, and replaces with a value slightly smaller than the minimum
+    """Finds missing values, and replaces them with a value slightly smaller than the minimum between both arrays.
 
     :param :class:'numpy.ndarray'x
     :param :class:'numpy.ndarray'y
     :param :py:class:'str'type
     """
     if type == 'local':
-        x_na = np.isnan(x)
-        y_na = np.isnan(y)
-        match_na = x_na & y_na
-        i = 0
-        while i < len(match_na):
-            if match_na[i]:
-                x = np.delete(x, i)
-                y = np.delete(y, i)
-            i += 1
-    xy = np.concatenate((x, y), axis=None)
-    na_replace = min(xy[~np.isnan(xy)]) - 0.1
-    x2 = x
-    y2 = y
-    x2[np.isnan(x)] = na_replace
-    y2[np.isnan(y)] = na_replace
+        match_na = np.logical_and(np.isnan(x), np.isnan(y))
+        x = x[np.logical_not(match_na)]
+        y = y[np.logical_not(match_na)]
 
-    np_out = sci.kendalltau(x2, y2)
+    na_replace = min(np.fmin(x, y)) - 0.1
+    np.nan_to_num(x, copy=False, nan=na_replace)
+    np.nan_to_num(y, copy=False, nan=na_replace)
+
+    np_out = sci.kendalltau(x, y)
     print(np_out)
 
 
