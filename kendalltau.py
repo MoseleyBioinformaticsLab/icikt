4d5ba4c532bd0344ec20e2576fced55a08d825e6
Praneeth Bhatt <psbhatt@ananke.cesb.uky.edu>
2021-06-11 16:12:19 -0400

    removed pandas import and replaced none values with np.nan

--- kendalltau.py
+++ kendalltau.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 import scipy.stats as sci
 
 
@@ -11,8 +10,8 @@ def runNumpyKT(x, y, type='global'):
     :param :py:class:'str'type
     """
     if type == 'local':
-        x_na = pd.isnull(x)
-        y_na = pd.isnull(y)
+        x_na = np.isnan(x)
+        y_na = np.isnan(y)
         match_na = x_na & y_na
         i = 0
         while i < len(match_na):
@@ -20,26 +19,24 @@ def runNumpyKT(x, y, type='global'):
                 x = np.delete(x, i)
                 y = np.delete(y, i)
             i += 1
-
-    min_xy = min(np.concatenate((x, y), axis=None)[np.concatenate((x, y), axis=None) != None])
-    na_replace = min_xy - 0.1
+    xy = np.concatenate((x, y), axis=None)
+    na_replace = min(xy[~np.isnan(xy)]) - 0.1
     x2 = x
     y2 = y
-    x2[pd.isnull(x)] = na_replace
-    y2[pd.isnull(y)] = na_replace
+    x2[np.isnan(x)] = na_replace
+    y2[np.isnan(y)] = na_replace
 
-    np_out = sci.kendalltau(x2,y2)
+    np_out = sci.kendalltau(x2, y2)
     print(np_out)
 
 
 def main():
     """Created some test arrays to call runNumpyKT with
     """
-    x = np.array([None, 5, 3, None, None, 2])
-    y = np.array([4, 1, 17, 8, None, 6])
+    x = np.array([np.nan, 5, 3, np.nan, np.nan, 2])
+    y = np.array([4, 1, 17, 8, np.nan, 6])
     runNumpyKT(x, y, 'local')
 
 
 if __name__ == "__main__":
     main()
-
