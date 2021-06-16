985157d35c0b2f8ca9b37279e2545a4bbfe52cad
Praneeth Bhatt <psbhatt@ananke.cesb.uky.edu>
2021-06-16 17:41:14 -0400

    fixed restructured text and deleted commented code that had been replaced

--- kendalltau.py
+++ kendalltau.py
@@ -10,8 +10,11 @@ def iciKT(x, y, type='global'):
 
     :param :class:'numpy.ndarray'x
     :param :class:'numpy.ndarray'y
-    :param :py:class:'str'type
+    :param :py:class:'str'type: Default is 'global'.
+    :return: List with correlation and pvalue
+    :rtype: :py:class:'list'
     """
+
     if type == 'local':
         matchNA = np.logical_and(np.isnan(x), np.isnan(y))
         x = x[np.logical_not(matchNA)]
@@ -25,25 +28,27 @@ def iciKT(x, y, type='global'):
     return [corr, pval]
 
 
-def iciktArray(xyz, replaceVal=None):
-    """Calls runNumpyKT to calculate ICI-Kendall-Tau between every combination of
-    columns in the 2d array.
+def iciktArray(dataArray, replaceVal=None):
+    """Calls iciKT to calculate ICI-Kendall-Tau between every combination of
+    columns in the input 2d array, dataArray. Also replaces any instance of the replaceVal in the array with np.nan.
 
-    :param :class:'numpy.ndarray'xyz
+    :param :class:'numpy.ndarray'dataArray
+    :param replaceVal: Optional value to replace with np.nan. Default is None.
+    :type replaceVal: :py:class:'NoneType.None' or :py:class:'int' or :py:class:'double' or py:class:'float'
     :return: tuple of the correlations and pvalues 2d arrays
+    :rtype: :py:class:'tuple'
     """
-    if replaceVal is not None:
-        xyz[xyz == replaceVal] = np.nan
 
-    size = xyz.shape[1]
+    if replaceVal is not None:
+        dataArray[dataArray == replaceVal] = np.nan
+
+    size = dataArray.shape[1]
     corrArray, pvalArray = np.zeros([size, size]), np.zeros([size, size])
 
     # produces every combination of columns in the array
-    product = it.product(np.hsplit(xyz, size), np.hsplit(xyz, size))
-    # calls runNumpyKT to calculate ICIKendallTau for every combination in product and stores in a list
+    product = it.product(np.hsplit(dataArray, size), np.hsplit(dataArray, size))
 
-    # tempList = [iciKT(np.squeeze(i[0]), np.squeeze(i[1]), 'local') for i in product]
-
+    # calls iciKT to calculate ICIKendallTau for every combination in product and stores in a list
     with multiprocessing.Pool() as pool:
         tempList = pool.starmap(iciKT, ((*i, 'local') for i in product))
 
@@ -60,14 +65,9 @@ def iciktArray(xyz, replaceVal=None):
 def main():
     """Created some test arrays to call runNumpyKT with
     """
-    # x = np.array([np.nan, 5, 3, np.nan, np.nan, 2])
-    # y = np.array([4, 0, 17, 8, np.nan, 6])
-    # z = np.array([6, 10, np.nan, 3, 9, 14])
-    # xyz = np.column_stack((x, y, z))
-    # runNumpyKT(x, y, 'local')
-    # corr, pval = iciktArray(xyz, replaceVal=6)
+
     largeArray = np.genfromtxt('D:/large_transcript.csv', delimiter=",")
-    tmpArray = largeArray[..., 0:50]
+    tmpArray = largeArray[..., :50]
 
     sTime = time.time()
     corr, pval = iciktArray(tmpArray, replaceVal=0)
