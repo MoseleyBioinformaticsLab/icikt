9bb24cc69a15f61a45e539b670ea27f60fa6057d
Praneeth Bhatt <psbhatt@ananke.cesb.uky.edu>
2021-06-17 13:42:48 -0400

    separated small and big test into 2 functions

--- kendalltau.py
+++ kendalltau.py
@@ -63,14 +63,31 @@ def iciktArray(dataArray, replaceVal=None):
 
 
 def main():
-    """Created some test arrays to call runNumpyKT with
-    """
+    bigTest()
 
-    largeArray = np.genfromtxt('D:/large_transcript.csv', delimiter=",")
-    tmpArray = largeArray[..., :50]
+
+def smallTest():
+
+    x = np.array([np.nan, 5, 3, np.nan, np.nan, 2])
+    y = np.array([4, 0, 17, 8, np.nan, 6])
+    z = np.array([6, 10, np.nan, 3, 9, 14])
+    xyz = np.column_stack((x, y, z))
 
     sTime = time.time()
-    corr, pval = iciktArray(tmpArray, replaceVal=0)
+    corr, pval = iciktArray(xyz, replaceVal=0)
+    fTime = time.time()
+
+    print(corr)
+    print(pval)
+    print(fTime - sTime)
+
+
+def bigTest():
+
+    largeArray = np.genfromtxt('D:/bigTest.csv', delimiter=",")
+
+    sTime = time.time()
+    corr, pval = iciktArray(largeArray)
     fTime = time.time()
 
     print(corr)
