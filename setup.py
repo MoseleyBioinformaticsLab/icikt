0cae1a39f97a2da275866ced2ad1b1c9f3354b2e
Praneeth Bhatt <psbhatt@ananke.cesb.uky.edu>
2021-06-24 13:02:55 -0400

    edited setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import re
+from setuptools import setup, find_packages
+
+if sys.argv[-1] == 'publish':
+    os.system('python3 setup.py sdist')
+    os.system('twine upload dist/*')
+    sys.exit()
+
+
+def readme():
+    with open('README.rst') as readme_file:
+        return readme_file.read()
+
+
+def find_version():
+    with open('pyicikendalltau/__init__.py', 'r') as fd:
+        version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
+    if not version:
+        raise RuntimeError('Cannot find version information')
+    return version
+
+
+REQUIRES = [
+    "numpy >= 1.18.2",
+    "scipy >= 1.4.1"
+]
+
+
+setup(
+    name='pyicikendalltau',
+    version=find_version(),
+    author='Praneeth S. Bhatt, Robert M. Flight, Hunter N.B. Moseley',
+    author_email='praneethsbhatt@gmail.com',
+    description='Parser for mwtab files from the Metabolomics Workbench',
+    keywords='mwtab metabolomics workbench',
+    license='Modified Clear BSD',
+    url='https://gitlab.cesb.uky.edu/rmflight/pythonICIKendallTau',
+    packages=find_packages(),
+    platforms='any',
+    long_description=readme(),
+    install_requires=REQUIRES,
+)
