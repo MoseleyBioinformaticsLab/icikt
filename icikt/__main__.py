f252e3e36ef3b6f7a861f514bae8c8b2bf352d6a
Praneeth Bhatt <psbhatt@ananke.cesb.uky.edu>
2021-10-26 11:37:14 -0400

    updating cli

--- icikt/__main__.py
+++ icikt/__main__.py
@@ -4,14 +4,17 @@ Python Information-Content-Informed Kendall Tau Correlation (ICIKT)
 
 
 Usage:
-    icikt.py iciktArray <dataFilePath> [--mode=<type>] [--data-format=<format>] [--replace=<globalNA>]
+    icikt.py iciktArray <dataFilePath> [--data-format=<format>] [--replace=<globalNA>] [--mode=<perspective>] [--scale=<scaleMax>] [--diag=<diagGood>]
     icikt.py -h | --help
 
 Options:
     -h, --help                      Shows this screen.
-    --mode=<type>                 Options are global or local [default: global].
-    --data-format=<format>          Input file format, available formats: csv, tsv [default: csv].
-    --replace=<globalNA>        Value to be replaced with nan.
+    --data-format=<format>      Input file format, available formats: csv, tsv [default: csv].
+    --replace=<globalNA>        Value to be replaced with nan [default: 0].
+    --mode=<perspective>        Options are global or local [default: global].
+    --scale=<scaleMax>          Should everything be scaled compared to the maximum correlation [default: TRUE]?
+    --diag=<diagGood>           Should the diagonal entries reflect how many entries in the sample were "good" [default:TRUE]?
+
 """
 
 from . import icikt
@@ -33,7 +36,7 @@ def main():
         if args["--replace"] is not None:
             args["--replace"] = float(args["--replace"])
 
-        icikt.iciktArray(args["<dataFilePath>"], args["--replace"], args["--mode"])
+        icikt.iciktArray(args["<dataFilePath>"], args["--replace"], args["--mode"], args["--scale"], args["--diag"])
 
 
 if __name__ == "__main__":
