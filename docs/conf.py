bfcf8b0ec95c921d1f8d50e3062aa4b69ed77647
Praneeth Bhatt <psbhatt@ananke.cesb.uky.edu>
2022-01-07 12:55:11 -0500

    fixed kendall_dis name

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
-autodoc_mock_imports = ['kendall_dis']
+autodoc_mock_imports = ['_kendall_dis']
 
 
 # -- Project information -----------------------------------------------------
