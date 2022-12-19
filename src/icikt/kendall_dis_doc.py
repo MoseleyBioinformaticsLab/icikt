7bf091b303c51f30692ebcbd17943b3b30d58a34
Praneeth Bhatt <praneethsbhatt@gmail.com>
2022-12-19 11:57:42 -0500

    created kendall_dis_doc file to generate documentaiton for the kendall dis funciton when Cython isn't properly installed

---
+++ src/icikt/kendall_dis_doc.py
@@ -0,0 +1,5 @@
+# placeholder function and file for generating documentation for the cythonized function kendall_dis()
+
+def kendall_dis(x, y):
+    print("Cython package is not properly installed")
+    pass
