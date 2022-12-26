c34e8596caad745a2c65a39c7357c4e8c65dcf74
Praneeth Bhatt <praneethsbhatt@gmail.com>
2022-12-26 09:48:18 -0500

    fixed kendall dis install

--- src/icikt/__init__.py
+++ src/icikt/__init__.py
@@ -1,6 +1,9 @@
 __version__ = "1.2.0"
 
-from . import _kendall_dis
+try:
+        from . import _kendall_dis
+except ImportError:
+        from . import kendall_dis_doc as _kendall_dis
 from .icikt import icikt
 from .icikt import iciktArray
 
