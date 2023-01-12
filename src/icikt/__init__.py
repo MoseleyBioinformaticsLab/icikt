6c5c1ac3fcbdee292012e02abbe42d80fb19465b
Praneeth Bhatt <praneethsbhatt@gmail.com>
2023-01-12 12:01:29 -0500

    fixed imports to match refactoring of kendall_dis

--- src/icikt/__init__.py
+++ src/icikt/__init__.py
@@ -3,7 +3,7 @@ __version__ = "1.2.0"
 try:
         from . import _kendall_dis
 except ImportError:
-        from . import kendall_dis_doc as _kendall_dis
+        from . import kendall_dis as _kendall_dis
 from .icikt import icikt
 from .icikt import iciktArray
 
