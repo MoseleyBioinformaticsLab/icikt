2520d62173942239bd6a0e7c9b590550f9aeffe0
Praneeth Bhatt <praneethsbhatt@gmail.com>
2023-01-12 12:08:43 -0500

    renamed kendall_dis_doc

---
+++ src/icikt/kendall_dis.py
@@ -0,0 +1,7 @@
+# placeholder function and file for generating documentation for the cythonized function kendall_dis()
+import typing as T
+
+
+def kendall_dis(x, y):
+    print("Cython package is not properly installed")
+    pass
