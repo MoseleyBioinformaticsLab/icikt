659d07929f04ad855b1adce53fc90d3f266d21d5
Praneeth Bhatt <praneethsbhatt@gmail.com>
2024-08-20 13:59:13 -0400

    change cython requirement and added proper exiting to kendall_dis.py

--- src/icikt/kendall_dis.py
+++ src/icikt/kendall_dis.py
@@ -1,7 +1,7 @@
 # placeholder function and file for generating documentation for the cythonized function kendall_dis()
-import typing as T
+import logging
 
 
 def kendall_dis(x, y):
-    print("Cython package is not properly installed")
-    pass
+    logging.error("Cython package is not properly installed")
+    sys.exit(1)
