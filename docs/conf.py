a9faf73fb982b47cb33130d89a6708c45abe4874
Praneeth Bhatt <praneethsbhatt@gmail.com>
2024-08-30 13:40:26 -0400

    added sphinxs_rtd_theme to conf and requirements

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,8 @@ version = __version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = ['sphinx_rtd_theme',
+    'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
